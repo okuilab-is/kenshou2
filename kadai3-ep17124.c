329fd13a9632f094e80883213c937d236cdbcb87
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-11 15:46:45 +0900

    gccss

--- kadai3-ep17124.c
+++ kadai3-ep17124.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int main(void){
-   int m, n, a[100], b[100];
+   int m, n, a[100] = {}, b[100] = {}, i, j;
 
    scanf("%d", &m);
-   for (int i = 0; i < m ; i++) scanf("%d ", &a[i]);
+   for (i = 0; i < m ; i++) scanf("%d ", &a[i]);
    scanf("%d", &n);
-   for (int j = 0; j < n ; j++) scanf("%d ", &b[j]);
+   for (j = 0; j < n ; j++) scanf("%d ", &b[j]);
 
    i = 0, j = 0;
    for (int k = 0; k < m + n; k++){
@@ -15,7 +15,8 @@ int main(void){
          i++;
       } else {
          printf("%d", b[j]);
-        j++;
+         j++;
+      }
    }
    printf("\n");
 
