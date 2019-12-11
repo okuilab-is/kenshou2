99b803db75d602f021a581795dca88e21ef93f99
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-11 16:12:37 +0900

    gccss

--- kadai3-ep17124.c
+++ kadai3-ep17124.c
@@ -4,9 +4,9 @@ int main(void){
    int m, n, a[100] = {}, b[100] = {}, i, j;
 
    scanf("%d", &m);
-   for (i = 0; i < m ; i++) scanf("%d ", &a[i]);
+   for (i = 0; i < m ; i++) scanf("%d", &a[i]);
    scanf("%d", &n);
-   for (j = 0; j < n ; j++) scanf("%d ", &b[j]);
+   for (j = 0; j < n ; j++) scanf("%d", &b[j]);
 
    i = 0, j = 0;
    for (int k = 0; k < m + n; k++){
