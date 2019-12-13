832c76b61836c05f25c9c471b12e28c9fd80006d
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-13 17:17:29 +0900

    gccss

--- kadai3-EP17123.c
+++ kadai3-EP17123.c
@@ -3,9 +3,9 @@
 int main(void){
 	int i,m,n,x[100],y[100],xi=0,yi=0;
 	scanf("%d",&m);
-	for(i=0; i<m; i--) scanf("%d",x);
+	for(i=0; i<m; i++) scanf("%d",x);
 	scanf("%d",&n);
-	for(i=0; i<n; i--) scanf("%d",y);
+	for(i=0; i<n; i++) scanf("%d",y);
 	printf("%d",x[0]);
 	/*for(i=0; i<m+n; i++){
 		if(x[xi]<y[yi]){
