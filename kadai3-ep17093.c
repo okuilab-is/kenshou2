ff2486927139a743ccc96a8fe06e2dd3ed2274ae
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-16 11:20:01 +0900

    gccss

--- kadai3-ep17093.c
+++ kadai3-ep17093.c
@@ -3,9 +3,10 @@
 void main(){
   int m,n;
   scanf("%d",&n);
+  int a[m];
+  for(int i=0;i<m; i++) scanf("%d",&a[i]);
   scanf("%d",&n);
-  int a[m],b[n];
-  for(int i=0; i<m; i++) scanf("%d",&a[i]);
+  int b[n];
   for(int i=0; i<n; i++) scanf("%d",&b[i]);
   int j=0,k=0;
   for(int i=0; i<m+n; i++){
