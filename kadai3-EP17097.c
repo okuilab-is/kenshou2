21c4a99559429f51af0ec0854a834ac88c179abd
hogehoge <ep16018@sti.chubu.ac.jp>
2019-12-16 15:30:57 +0900

    gccss

--- kadai3-EP17097.c
+++ kadai3-EP17097.c
@@ -2,11 +2,23 @@
 
 int main(void){
 	int m=0,n=0,i,j,a[m],b[n];
-	scanf("%d",m);
+	scanf("%d",&m);
 	for(i=0;i<m;i++){
 		scanf("%d",&a[i]);
 	}
-
-
+	scanf("%d",&n);
+	for(j=0;j<n;j++){
+		scanf("%d",&b[j]);
+	}
+	for(k=0;k<m;k++){
+		for(l=0;l<m;l++){
+			if(a[k]<b[l]){
+				printf("%d",a[k]);
+				k++;
+			}else{
+				printf("%d",b[l]);
+			}
+		}
+	}
 
 }
